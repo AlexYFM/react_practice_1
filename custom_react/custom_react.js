0ab9f16015eb1c6fd7efaa3d94ae63c44d2f676e
Alex Yuan <alexyuan0.ay@gmail.com>
2024-01-16 17:55:59 -0600

    A bit more practice

---
+++ custom_react/custom_react.js
@@ -0,0 +1,10 @@
+const react_element = {
+    type: 'a',
+    props: {
+        href: "google.com",
+        target: "_blank"
+    },
+    children: 'Click me to visit google'
+}
+
+const root = document.getElementById('root')
