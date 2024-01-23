3727ec2a2e64be51d8e45f4ea1c843136fe65aad
Alex Yuan <alexyuan0.ay@gmail.com>
2024-01-22 18:15:33 -0600

    finished lecture/project' :q

---
+++ 07_react_router/src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+const Github = () => {
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/AlexYFM')
+    //     .then((res) => res.json())
+    //     .then((data) => setData(data))
+    // }, [])
+
+    const data = useLoaderData()
+
+    return (
+        <div className=' text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+            <img src={data.avatar_url} alt='avatar'/>
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const res = await fetch('https://api.github.com/users/AlexYFM')
+    return res.json()
+}
--- 07_react_router/src/main.jsx
+++ 07_react_router/src/main.jsx
@@ -4,14 +4,23 @@ import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from '../Layout.jsx'
-import Home from '../components/Home/Home.jsx'
-import About from '../components/About/About.jsx'
+import Home from '../src/components/Home/Home.jsx'
+import About from '../src/components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, {githubInfoLoader} from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
+  createRoutesFromElements( //the routes below work like case statments, so wildcard needs to be at end
     <Route path = '/' element = {<Layout/>}>
       <Route path='' element = {<Home/>}/>
       <Route path='about' element = {<About/>}/>
+      <Route path='contact' element = {<Contact/>}/>
+      <Route path='user/' element = {<User/>}>
+        <Route path=':userid' element = {<User/>} />
+      </Route>
+      <Route path='github' element = {<Github/>} loader={githubInfoLoader}/>
+      <Route path="*" element={<div className=' text-center text-3xl py-5'>Not Found</div>}/>
     </Route>
   )
 )
